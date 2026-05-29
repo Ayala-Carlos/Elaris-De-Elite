35f96d47b4353fe2f5cfc71d9d6b45633671cc5f
julio <20210261@ricaldone.edu.sv>
2026-05-29 10:30:37 -0600

    feat: crud categories terminado

---
+++ backend/src/routes/categories.js
@@ -0,0 +1,23 @@
+import express from "express"
+import categoriesController from "../controllers/categoriesController.js";
+
+const router = express.Router();
+
+//(api/categories/)
+router.route("/")
+.get(categoriesController.getCategories)
+.post(categoriesController.createCategory)
+
+//Additional endpoints for specific category operations
+
+router.route("/searchByCode").post(categoriesController.searchByCodeAndIsAvailable)
+
+router.route("/count").get(categoriesController.countCategories)
+
+//Define the endpoints for update and delete a category, the id of the category is send in the url as a parameter
+//(api/categories/:id)
+router.route("/:id")
+.put(categoriesController.updateCategory)
+.delete(categoriesController.deleteCategory)
+
+export default router;
