ce975e728fa296febbea5ee3e865d109f72c4fc5
Julio-Perez32 <20210261@ricaldone.edu.sv>
2026-05-31 17:16:25 -0600

    feat: crud terminado de reviews

---
+++ backend/src/models/reviews.js
@@ -0,0 +1,36 @@
+import mongoose, { Schema, model } from "mongoose"
+
+const reviewsSchema = new Schema({
+    idOrder: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "orders"
+    },
+    idClient:{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "client"
+    },
+    rating:{
+        type: Number,
+        min: 1,
+        max: 5
+    },
+    comment:{
+        type: String
+    },
+    reviewDate:{
+        type: Date
+    },
+    status:{
+        type: String,
+        enum: ["active", "inactive"],
+        default: "active"
+    }
+}, {
+    // timestamps: true, agrega automáticamente campos de fecha de creación y actualización a los documentos de la colección, lo que facilita el seguimiento de cuándo se crearon o modificaron los registros.
+    timestamps: true,
+    // strict: false, permite agregar campos adicionales a los documentos de la colección, lo que facilita la flexibilidad en la estructura de los datos.
+    strict: false
+})
+
+//"reviews" es el nombre de la colección que se guarda en la DB
+export default model("reviews", reviewsSchema)
